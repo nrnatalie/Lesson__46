09c4b3ed6f920ce1952bb791e912cb10d1f3b368
Nathalie Russo <johndoe@example.com>
2023-03-23 15:53:34 +0200

    Add constructor

--- src/Homework__45/Book.java
+++ src/Homework__45/Book.java
@@ -9,11 +9,15 @@ package Homework__45;
     //Создайте компаратор, который позволит сортировать книги по убыванию количества страниц,
       //  а при совпадающем объёме - по названиям ("по возрастанию", по алфавиту).
 public class Book {
-  private String author;
-  private String title;
-  private int pages;
+  private final String author;
+  private final String title;
+  private final int pages;
 
   public Book(String author, String title, int pages){
+
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
 
   }
 
