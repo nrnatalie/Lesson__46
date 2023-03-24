2f20322c60b03394a802b89e396f0118ea937b83
Nathalie Russo <johndoe@example.com>
2023-03-24 12:37:14 +0200

    Update Calculator Tests

--- src/CalculatorTest.java
+++ src/CalculatorTest.java
@@ -5,20 +5,34 @@ import java.util.concurrent.Callable;
 
 
 public class CalculatorTest {
+  private Calculator calc = new Calculator();
 
   @Test
   // public void Add_2Plus5_7Returned //тестирую метод add
-  public void addPositiveIntegers(){
+  public void addPositiveIntegers() {
     // orrange упорядочить установить
-    Calculator calc = new Calculator();
+    //Calculator calc = new Calculator();
 
     //act- выполнить действие
-    int result = calc.add(2,5);
+    int result = calc.add(2, 5);
     // assert- сравнить результат с правильным
-    assertEquals(7,result);
+    assertEquals(7, result);
 
+    // или одной формулой все 3 этапа
+    assertEquals(7, calc.add(2, 5));
 
-      }
-    };
+  }
+
+  @Test
+  public void addNegativeIntegers() {
+    assertEquals(-5, calc.add(-2, -3));
+    assertEquals(-5, calc.add(-3,-2));
+  }
+  @Test
+  public void addNegativZero(){
+    assertEquals(-5, calc.add(-5,0));
+    assertEquals(-5,calc.add(0,-5));
+  }
+}
 
 
